580786a0e6e3a3c33a16547e87d680f0615bae99
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-08-10 22:40:53 -0300

    Routing implement in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.scss';
-import SimpleTabs from './components/SimpleTabs/SimpleTabs';
+import { Provider } from 'react-redux';
+import store from './store/storeConfig';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home';
+import GridData from './components/GridData/GridData';
+import { Container } from '@material-ui/core';
+import Header from './components/Header/Header';
+import Page404 from './pages/Page404'
 
 function App() {
   return (
-    <div className="App">
-      <SimpleTabs />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Container>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/animes">
+              <GridData />
+            </Route>
+            <Route>
+              <Page404 />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    </Provider>
   );
 }
 
