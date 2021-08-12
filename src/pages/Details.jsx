7a241360e609a319235948298a43804ab024db5a
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-08-11 22:29:20 -0300

    pagina para representar os detalhes individuais

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,19 +1,28 @@
 import {
   Container,
   Card,
-  CardMedia,
+  // CardMedia,
   CardContent,
   Typography,
 } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAnimeDetail } from "../api/api";
 
 function Details(props) {
+  const [data, setData] = useState(props.data);
+  useEffect(() => {
+    const animeInfo = fetch(
+      getAnimeDetail(props.id).then((data) => console.log(data))
+    );
+  }, []);
+  console.log(data);
+
   return (
     <Container>
       <Card>
-        <CardMedia>
+        {/* <CardMedia>
           <img src={props.image} alt="image" />
-        </CardMedia>
+        </CardMedia> */}
         <CardContent>
           <Typography variant="h4" component="h2">
             {props.title}
