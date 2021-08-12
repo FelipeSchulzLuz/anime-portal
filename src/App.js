8309d1db13948f382321e2a2088d7af8207feb7b
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-08-11 22:25:49 -0300

    remoção de provider direto no App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import './App.scss';
-import { Provider } from 'react-redux';
-import store from './store/storeConfig';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home';
 import GridData from './components/GridData/GridData';
 import { Container } from '@material-ui/core';
 import Header from './components/Header/Header';
 import Page404 from './pages/Page404'
+import Details from './pages/Details';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Header />
-        <Container>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/animes">
-              <GridData />
-            </Route>
-            <Route>
-              <Page404 />
-            </Route>
-          </Switch>
-        </Container>
-      </Router>
-    </Provider>
+    <Router>
+      <Header />
+      <Container>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/animes">
+            <GridData />
+          </Route>
+          <Route path="/anime/:id">
+            <Details />
+          </Route>
+          <Route>
+            <Page404 />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
+
+
 
 export default App;
