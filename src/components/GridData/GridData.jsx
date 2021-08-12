e6e27d235899a93b2365c68149155ffab79ce473
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-08-12 16:12:13 -0300

    dados chegando da requisição da api

--- src/components/GridData/GridData.jsx
+++ src/components/GridData/GridData.jsx
@@ -1,142 +1,22 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
-import clsx from "clsx";
-import { lighten, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
 import { useEffect } from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import { Route } from "react-router-dom";
 import { actions } from "../../actions/animes";
+import { getComparator, stableSort } from "./../../utils/utils";
+import { EnhancedTableToolbar } from "./../EnhancedTableToolbar/EnhancedTableToolbar";
+import { EnhancedTableHead } from "./../EnhancedTableHead/EnhancedTableHead";
 import { getAnimeList } from "./../../api/api";
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  { id: "mal_id", label: "Id", minWidth: 170 },
-  { id: "title", label: "Title", minWidth: 170 },
-  { id: "season_year", label: "Season Year", minWidth: 50, numeric: true },
-  { id: "start_date", label: "Start Date", minWidth: 50 },
-];
-function EnhancedTableHead(props) {
-  const { classes, order, orderBy, onRequestSort } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-const useToolbarStyles = makeStyles((theme) => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-    theme.palette.type === "light"
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  title: {
-    flex: "1 1 100%",
-  },
-}));
-
-const EnhancedTableToolbar = (props) => {
-  const classes = useToolbarStyles();
-
-  return (
-    <Toolbar className={clsx(classes.root)}>
-      <Typography
-        className={classes.title}
-        variant="h6"
-        id="tableTitle"
-        component="span"
-      >
-        Sort the animes clicking on the column header
-      </Typography>
-    </Toolbar>
-  );
-};
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -163,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function GridData(props, getAnimeList, setAnimeCode) {
+function GridData() {
   const classes = useStyles();
   const [order, setOrder] = useState("asc");
   const [orderBy, setOrderBy] = useState("title");
@@ -172,11 +52,24 @@ function GridData(props, getAnimeList, setAnimeCode) {
   const [dense, setDense] = useState(false);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [data, setData] = useState([]);
-  const dispatch = useDispatch();
+
+  const animeList = async () => {
+    const result = await getAnimeList()
+    setData(result);
+  };
+
+  //////////////////////////////////////////////
 
   useEffect(() => {
-    dispatch(actions.getAnimeList);
+    if (data.length === 0) {
+      animeList();
+    }
+    // setData(props.anime);
+    console.log("DATA IN ", data);
   });
+  console.log("DATA OUT", data);
+
+  /////////////////////////////////////////////
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
@@ -279,12 +172,12 @@ function GridData(props, getAnimeList, setAnimeCode) {
 }
 
 const mapStateToProps = (state) => ({
-  animes: state.animes,
+  anime: state.anime,
+  animeCode: state.animeCode,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  GetAnimeList: () => dispatch(actions.GetAnimeList),
-  SetAnimeCode: () => dispatch(actions.SetAnimeCode),
-});
+const mapDispatchToProps = {
+  anime: actions.getAnimeList(),
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(GridData);
