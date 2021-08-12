aec51da75920e15516f8e68c87dd4522e9fb04c4
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-08-11 22:26:18 -0300

    anime action

---
+++ src/actions/animes.js
@@ -0,0 +1,11 @@
+const actions = {
+    getAnimeList: () => ({
+        type: 'GET_ANIME_LIST',
+    }),
+
+    setAnimeCode: () => ({
+        type: 'SET_ANIME_CODE',
+    })
+}
+
+export { actions };
