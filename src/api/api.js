9ca7e2a30da5bc6f53048bdb913b67b0cacaeeee
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-08-12 20:48:15 -0300

    add new components and

--- src/api/api.js
+++ src/api/api.js
@@ -1,38 +1,56 @@
 import axios from "axios";
 
-export const getAnimeList = async () => {
-    const options = {
-        method: "GET",
-        url: "https://jikan1.p.rapidapi.com/user/Nekomata1037/animelist/all",
-        headers: {
-            "x-rapidapi-key":
-                "aabc1c15b8msh3e91db72b837037p1a1acdjsn5785d9abec05",
-            "x-rapidapi-host": "jikan1.p.rapidapi.com",
-        },
-    };
+const optionsGetList = {
+    method: "GET",
+    url: "/user/Nekomata1037/animelist/all",
+    headers: {
+        "x-rapidapi-key":
+            "aabc1c15b8msh3e91db72b837037p1a1acdjsn5785d9abec05",
+        "x-rapidapi-host": "jikan1.p.rapidapi.com",
+    },
+};
+// export const getAnimeList = async () => {
+//     try {
+//         const animeList = await axios(optionsGetList);
+//         console.log(animeList.data.anime)
+//         return animeList.data.anime;
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+
+
+// const optionsGetDetail = {
+//     method: 'GET',
+//     headers: {
+//         'x-rapidapi-key': 'aabc1c15b8msh3e91db72b837037p1a1acdjsn5785d9abec05',
+//         'x-rapidapi-host': 'jikan1.p.rapidapi.com'
+//     }    
+// };    
+
+const getApi = axios.create({
+    baseURL: "https://jikan1.p.rapidapi.com/user/Nekomata1037",
+})    
+
+
+export const getAnimeList = async (id) => {
     try {
-        const data = await axios.request(options);
-        return data.data.anime;
+        const resultGetAnimeList = await getApi(`/animelist/all`, optionsGetList).then(res => res.data.anime);
+        // const data = await axios.request(...optionsGetOne, url);
+        // console.log("getAnimeList", resultGetAnimeList);
+        return resultGetAnimeList;
     } catch (e) {
         console.log(e);
     }
 }
 
-
-export const getAnimeDetail = async (id) => {
-    const options = {
-        method: 'GET',
-        url: `https://jikan1.p.rapidapi.com/anime/${id}/news`,
-        headers: {
-            'x-rapidapi-key': 'aabc1c15b8msh3e91db72b837037p1a1acdjsn5785d9abec05',
-            'x-rapidapi-host': 'jikan1.p.rapidapi.com'
-        }
-    };
-    try {
-        const data = await axios.request(options);
-        console.log("ONE", data);
-        return data.data;
-    } catch (e) {
-        console.log(e);
-    }
-}
+// export const getAnimeDetail = async (id) => {
+//     const url = { url: `https://jikan1.p.rapidapi.com/anime/${id}/news` }
+//     try {
+//         const data = await axios.request(...optionsGetOne, url);
+//         console.log("ONE", data);
+//         return data.data;
+//     } catch (e) {
+//         console.log(e);
+//     }
+//
